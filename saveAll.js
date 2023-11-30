24690bae13485d55a0845a1a6d7bdd2143cefd69
Ben Carruthers <ben_carruthers@hotmail.co.uk>
2023-11-30 16:15:18 +0000

    Bug fix

--- saveAll.js
+++ saveAll.js
@@ -1,14 +1,15 @@
 (function () {
   var docs = app.documents;
+  var saved = 0;
   if (docs.length < 1) return;
   if (!confirm("Do you want to save all open documents?")) return;
-  if (!confirm("Are you sure you want to save ALL open documents?")) return;
   for (var i = 0; i < docs.length; i++) {
     var doc = docs[i];
     try {
       doc.save();
-    } catch (error) {
-      alert("There was an error:\n" + error);
-    }
+      saved++;
+    } catch (_) {}
   }
+  var problems = docs.length - saved;
+  if (problems > 0) alert("Problem with " + problems + " document(s)");
 })();
