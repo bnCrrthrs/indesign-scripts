ff03b76f9768680b2b297625fbf7e27a309f8fae
Ben Carruthers <ben_carruthers@hotmail.co.uk>
2024-01-05 23:28:33 +0000

    Added cols and rows input to fitImgsToGrid

--- fitImgsToGrid.js
+++ fitImgsToGrid.js
@@ -13,10 +13,28 @@
   var availableWidth = pageWidth - (marginLeft + marginRight);
   var availableHeight = pageHeight - (marginBottom + marginTop);
 
+  if (availableWidth <= 0) {
+    availableWidth = pageWidth;
+    marginLeft = 0;
+  }
+  if (availableHeight <= 0) {
+    availableHeight = pageHeight;
+    marginTop = 0;
+  }
+
   var rows = 4;
   var cols = 2;
+  var maxRows, maxCols;
 
-  //+ Select Files dialog
+  if (gutter === 0) {
+    maxRows = Math.floor(availableHeight);
+    maxCols = Math.floor(availableWidth);
+  } else {
+    maxRows = Math.floor(availableHeight / gutter);
+    maxCols = Math.floor(availableWidth / gutter);
+  }
+
+  //+ Select Folder dialog
   var window = new Window("dialog", "Fit images to grid");
   window.alignChildren = "fill";
 
@@ -30,7 +48,18 @@
   folderName.preferredSize = [250, -1];
 
   // choose rows / columns
-  //  var panel = window.add("panel", undefined, "Rows and columns");
+  var panel = window.add("panel", undefined, "Grid layout");
+  group = panel.add("group");
+
+  group.add("statictext", undefined, "Columns: ");
+  var colInput = group.add("edittext", undefined, "2");
+
+  group.add("statictext", undefined, "Rows: ");
+  var rowInput = group.add("edittext", undefined, "4");
+
+  colInput.helpTip = "Maximum: " + maxCols;
+  colInput.characters = rowInput.characters = 4;
+  rowInput.helpTip = "Maximum: " + maxRows;
 
   // ok go
   group = window.add("group");
@@ -39,7 +68,7 @@
   var btnCancel = group.add("button", undefined, "Cancel");
   btnOK.enabled = false;
 
-  //+ Select Files functions
+  //+ Select Folder functions
   folderBtn.onClick = function () {
     //    var user = new Folder("~/");
     //    var f = user.selectDlg("Select folder"); //Dialog("Select files", isImg, true); // for windows, the second argument should be something like "*.jpg"
@@ -58,6 +87,30 @@
     window.close(0);
   };
 
+  colInput.onChange = function () {
+    var naturalNum = getNaturalNum(colInput.text);
+    if (!naturalNum || naturalNum > maxCols) {
+      colInput.text = cols;
+    } else {
+      cols = colInput.text = naturalNum;
+    }
+  };
+
+  rowInput.onChange = function () {
+    var naturalNum = getNaturalNum(rowInput.text);
+    if (!naturalNum || naturalNum > maxRows) {
+      rowInput.text = rows;
+    } else {
+      rows = rowInput.text = naturalNum;
+    }
+  };
+
+  function getNaturalNum(text) {
+    var value = Number(text);
+    if (!value || value < 0.5) return false;
+    return Math.round(value);
+  }
+
   function isImg(file) {
     var fullName = file.fullName;
     var lowerCaseName = fullName.toLowerCase();
@@ -73,6 +126,8 @@
   if (window.show() === 0) return;
 
   var contents = folder.getFiles(isImg);
+  rows = Number(rowInput.text) || rows;
+  cols = Number(colInput.text) || cols;
 
   if (contents.length === 0) return alert("No images found in this folder");
 
