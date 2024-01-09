9bc27bacdd563d576d820e7ac9c329e3cf90a3ee
Ben Carruthers <ben_carruthers@hotmail.co.uk>
2024-01-09 14:01:15 +0000

    created copyLinksToSubfolder
    Copies visible, linked graphics to a new folder

---
+++ copyLinksToSubfolder.js
@@ -0,0 +1,46 @@
+(function () {
+  if (app.documents.length === 0) return;
+  var doc = app.activeDocument;
+
+  var graphics = doc.allGraphics;
+  if (!graphics.length) return;
+
+  var folder = Folder.selectDialog();
+  if (!folder) return;
+
+  var copied = 0;
+  var failed = 0;
+
+  for (var i = 0; i < graphics.length; i++) {
+    var graphic = graphics[i];
+    var link = graphic.itemLink;
+    var target = File(folder.fsName + "/" + link.name);
+    if (
+      !graphic ||
+      !link ||
+      !graphic.isValid ||
+      !link.isValid ||
+      link.status === LinkStatus.LINK_MISSING ||
+      link.status === LinkStatus.LINK_INACCESSIBLE ||
+      link.status === LinkStatus.LINK_EMBEDDED ||
+      !graphic.itemLayer.visible ||
+      !graphic.parent.visible ||
+      !graphic.visible
+    )
+      continue;
+    try {
+      link.copyLink(target, "bc", true);
+      copied++;
+    } catch (_) {
+      failed++;
+    }
+  }
+
+  var successMsg = copied + " files copied";
+  var failedMsg = "";
+  if (failed) {
+    failedMsg = "\n" + failed + " failed";
+  }
+
+  alert(successMsg + failedMsg);
+})();
