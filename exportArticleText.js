8a0cc62d0a640ee4788efc4c7069b0d0ee42140d
Ben Carruthers <ben_carruthers@hotmail.co.uk>
2024-03-12 13:09:47 +0000

    improvements to exportArticleText.js for alt-text and removing wacify function

--- exportArticleText.js
+++ exportArticleText.js
@@ -20,17 +20,53 @@
     var article = articles[i];
     var members = article.articleMembers;
     textFile.writeln("");
+
     for (var j = 0; j < members.length; j++) {
       var item = members[j].itemRef;
-      var altText = item.objectExportOptions.customAltText;
       if (item.hasOwnProperty("contentType") && item.contentType == ContentType.TEXT_TYPE) {
-        var contents = item.parentStory.contents;
+        var contents = item.parentStory.contents.toString();
+        var containedItems = item.parentStory.allPageItems;
+        // WRITE STORY
+        //////////////////  PREVIOUSLY:
+        //////////////////  textFile.writeln(wacify(contents));
         textFile.writeln(contents);
+
+        //GET ANCHORED OBJET ALT TEXT
+        for (var k = 0; k < containedItems.length; k++) {
+          var containedItem = containedItems[k];
+          if (!containedItem.hasOwnProperty("objectExportOptions")) continue;
+          var containedAltText = containedItem.objectExportOptions.customAltText;
+          if (containedAltText) textFile.writeln(containedAltText);
+        }
+      } else {
+        // WRITE ALT TEXT IF NOT STORY
+        var altText = item.objectExportOptions.customAltText;
+        if (altText) textFile.writeln(altText);
       }
-      if (altText) textFile.writeln(altText);
     }
   }
 
   textFile.close();
   textFile.execute();
+
+  // function wacify(text) {
+  //   return (
+  //     text
+  //       .replace(/ /g, " ")
+  //       .replace(/\n/, " ")
+  //       .replace(/ {2,}/g, " ")
+  //       .replace(/ +([\n\r])/g, "$1")
+  //       .replace(/[\n\r]{2,}/g, "\n")
+  //       .replace(/support *[\n\r]+(\d{1,2}%) oppose *[\n\r]+/g, "support, while $1 oppose, ")
+  //       .replace(/agree *[\n\r]+(\d{1,2}%) disagree *[\n\r]+/g, "agree, while $1 disagree, ")
+  //       .replace(/[\n\r]([£€]?\d+(?:[,\.]\d+)*(?:%|(?: ?\w+?))) ([iy]n) 20(\d\d)/g, " ($1 $2 20$3)")
+  //       .replace(/[\n\r]([£€]?\d+(?:[,\.]\d+)*(?:%|(?: ?\w+?))) sa bhliain 20(\d\d)/g, " ($1 sa bhliain 20$2)")
+  //       .replace(/(20\d\d) *[\n\r](\d+%) *$/gm, "$2 yn $1")
+  //       .replace(/(\w)([\n\r])(\d+%?)([\n\r]|$)/g, "$1: $3 \n") // this one edited out before
+  //       .replace(/(0{3})[\n\r]((?:\d|x)+%)([\n\r]|$)/g, "$1 (or $2) \n") // this one edited out before
+  //       .replace(/(?<![\?\)] *)[\n\r](\d+%?,?\d*)( *[\n\r])/g, ": $1 \n") // this one edited out before
+  //       .replace(/(:[^:\n\r]+): ([^\n\r]+)/g, "$1 or $2") // this one edited out before
+  //       .replace(/ [\n\r]/g, "\n") + "\n"
+  //   );
+  // }
 })();
